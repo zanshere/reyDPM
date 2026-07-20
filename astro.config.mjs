ddd65c28e61a857262d45d461c1381ab8bf297ca
zanshere <fauzan224321@gmail.com>
2026-07-20 08:29:13 +0700

    chore: change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.PUBLIC_SITE_URL || 'https://reyvespadpm.vercel.app',
+  site: import.meta.env.PUBLIC_SITE_URL || 'https://vespadpmcibubur.vercel.app',
   integrations: [
     react(),
     swup({
